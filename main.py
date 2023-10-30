6d7a4712b66eb6efb4a617898b5205e7ed6cd094
Natalia Puerto <lapuertoc@unal.edu.co>
2023-10-30 18:03:14 -0500

    Adición de evaluación de los datos fusionados

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import csv
 from data_fusion.ihs.ihs import rgb_to_ihs
 from data_fusion.a_wavelet.a_wavelet import a_wavelet
-from data_fusion.evaluacion.evaluacion_calidad import test_de_calidad
+from data_fusion.evaluacion.evaluacion_calidad import test_full_references, test_no_references
 from skimage.exposure import match_histograms
 import cv2
 import numpy as np
@@ -27,13 +27,14 @@ spatial_src = read_tif_image(dir_file_original_images,spatial_imagen_name)
 spectral_src = read_tif_image(dir_file_original_images,spectral_imagen_name)
 
 #Leo la imagen y reesampling, el metadata me guarda los datos originales de la espectral
-
+print("Leyendo imágenes y haciendo resampling")
 inter_spectral, metadata = resampling_spectral(spectral_src,spatial_src.height,spatial_src.width,'bilinear') 
 spectral_src.close()
 
 # print(inter_spectral, metadata, spectral_src)
 
 #Cargar las bandas y se guardan como una lista de bandas separadas
+print("Cargar las bandas y se guardan como una lista de bandas separadas")
 bands = get_RGB_bands_satellite_ndarray_and_other_bands(inter_spectral)
 [red,green,blue] = bands[0:3]
 other_bands = bands[3:]
@@ -41,14 +42,17 @@ image_rgb = rgb_img(red,green,blue)
 # plt.imshow(image_rgb)
 
 # Creacion de la falsa pancromatica
+print("Creación de la falsa pancromática")
 pancromatica = get_pancromatica(spatial_src)
 spatial_src.close()
 pancromatica = pancromatica.astype(np.float64)
 
 #RGB TO IHS
+print("RGB a IHS")
 iv1v2 = rgb_to_ihs(image_rgb)
 
 #Ecualizar los histogramas
+print("Ecualizar los histogramas")
 pan_i = match_histograms(pancromatica,iv1v2[:,:,0])
 #cv2.imwrite(os.path.join(dir_file_proccesed_images,"Imagen_ecualizada.png"),pan_i.astype('int64'))
 
@@ -69,10 +73,12 @@ elif(option == "2"):
     pan_v1_v2[:,:,0] = N_INT
 
 #Conversión de IHS a RGB
+print("Conversión de IHS a RGB")
 mask_matrix_to_rgb = [[1 ,1/sqrt(6), 1/sqrt(6)],[1, 1/sqrt(6), -1/2],[1, -2/sqrt(6), 0]]
 merged_rgb_image = process_imag_to_another_model(pan_v1_v2,mask_matrix_to_rgb)
 
 #Guardar datos fusionados
+print("Guardando datos fusionados...")
 merged_rgb_image_8bits = merged_rgb_image.astype('uint8')
 new_spectral_image = merge_bands(merged_rgb_image_8bits,other_bands)
 
@@ -99,15 +105,26 @@ spectral_imagen = np.dstack([spectral_image_src.read(band) for band in ix_rgb_ba
 resultado_image_src = read_tif_image(dir_file_proccesed_images,resultado_name)
 resultado_imagen = np.dstack([resultado_image_src.read(band) for band in ix_rgb_bands])
 
-# indices_spacial = test_full_reference(spatial_imagen,resultado_imagen)
-# indices_spectral = test_full_reference(np.transpose(inter_spectral,(1,2,0)),resultado_imagen)
+#EVALUACIÓN
+indices_full_reference_spacial = test_full_references(spatial_imagen,resultado_imagen)
+indices_full_reference_spectral = test_full_references(np.transpose(inter_spectral,(1,2,0)),resultado_imagen)
 
-# with open(os.path.join(dir_file_proccesed_images,"indices_calidad_spacial.csv"), "w", newline="") as f:
-#     writer = csv.writer(f)
-#     for test in indices_spacial:
-#         writer.writerow(test)
+print("Calculo de indices espectrales full reference espacial")
+with open(os.path.join(dir_file_proccesed_images,"indices_calidad_espacial_full_references.csv"), "w", newline="") as f:
+    writer = csv.writer(f)
+    for test in indices_full_reference_spacial:
+        writer.writerow(test)
 
-# with open(os.path.join(dir_file_proccesed_images,"indices_calidad_spectral.csv"), "w", newline="") as f:
-#     writer = csv.writer(f)
-#     for test in indices_spectral:
-#         writer.writerow(test)
+print("Calculo de indices espectrales full reference espectral")
+with open(os.path.join(dir_file_proccesed_images,"indices_calidad_espectral_full_references.csv"), "w", newline="") as f:
+    writer = csv.writer(f)
+    for test in indices_full_reference_spectral:
+        writer.writerow(test)
+
+indices_full_no_reference = test_no_references(np.transpose(inter_spectral,(1,2,0)),spatial_imagen,resultado_imagen)
+
+print("Calculo de indices espectrales no full reference")
+with open(os.path.join(dir_file_proccesed_images,"indices_calidad_no_full_references.csv"), "w", newline="") as f:
+    writer = csv.writer(f)
+    for test in indices_full_no_reference:
+        writer.writerow(test)
