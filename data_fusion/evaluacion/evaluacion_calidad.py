3ae1ad32540a24375418e4b0bc82da5a0876ee75
Natalia Puerto <lapuertoc@unal.edu.co>
2023-12-06 09:53:07 -0500

    Cambios en el ingreso de los parámetros en la evaluación no reference

--- data_fusion/evaluacion/evaluacion_calidad.py
+++ data_fusion/evaluacion/evaluacion_calidad.py
@@ -1,5 +1,6 @@
 from sewar.full_ref import uqi, ergas, sam
 from sewar.no_ref import d_lambda, d_s
+import numpy as np
 
 def test_full_references(original_imagen,proccesed_imagen):
     print('Calculando índices de calidad full references. Por favor espera...')
@@ -7,8 +8,8 @@ def test_full_references(original_imagen,proccesed_imagen):
             ("ERGAS", ergas(original_imagen, proccesed_imagen)),
             ("SAM: ", sam(original_imagen, proccesed_imagen))]
 
-def test_no_references(spectral_imagen,spatial_image,proccesed_imagen):
+def test_no_references(spectral_imagen,pancromatica,proccesed_imagen,):
     print('Calculando índices de calidad no references. Por favor espera...')
     return [("D_Lambda", d_lambda(spectral_imagen, proccesed_imagen)),
-            ("D_S", d_s(spatial_image, spectral_imagen, proccesed_imagen))]
+            ("D_S", d_s(pancromatica, np.transpose(spectral_imagen,(1,0,2)), proccesed_imagen,q=1,r=1,ws=1))]
 
