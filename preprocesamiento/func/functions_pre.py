8dc91e3a8148aa13a41adc47907f416f2ab1ad3f
Natalia Puerto <lapuertoc@unal.edu.co>
2024-01-17 18:13:05 -0500

    Funciones preprocesamiento completo

--- preprocesamiento/func/functions_pre.py
+++ preprocesamiento/func/functions_pre.py
@@ -1,13 +1,15 @@
 import os
 import numpy as np
 import rasterio
+import geopandas as gpd
 from func.functions import read_tif_image
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 from rasterio.merge import merge
 from rasterio.enums import Resampling
 from rasterio.mask import mask
 import fiona
-import cv2
+import matplotlib.pyplot as pyplot
+import imageio as imageio
 
 def transformacion_8bit(directorio_originales, directorio_destino):
     """ Aplica una transformación 8-bit a las imágenes en el directorio de origen y guarda las imágenes en el directorio de destino.
@@ -26,8 +28,7 @@ def transformacion_8bit(directorio_originales, directorio_destino):
             metadata.update(
                 dtype=rasterio.uint8,
                 count=1,
-                compress='lzw',
-                nodata=255,
+                nodata=0
                 )
             datos_imagen = imagen_src.read(1)
             valor_max = np.max(datos_imagen, where=(datos_imagen < 65535), initial=0)
@@ -116,9 +117,11 @@ def desconcatenar_bandas_uav(directorio_imagenes, nombre_archivo, directorio_sal
     blue = bandas[2, :, :]
     print(red.shape)
     # Guarda cada banda por separado
-    for nombre_banda, banda in zip(['red', 'green', 'blue'], [red, green, blue]):
+    for nombre_banda, banda in zip(['Banda_R', 'Banda_G', 'Banda_B'], [red, green, blue]):
         meta_banda = meta.copy()
-        meta_banda.update(count=1)
+        meta_banda.update(
+            count=1
+            )
         # Actualiza el nombre del archivo de salida con el nombre de la banda
         nombre_salida = os.path.join(directorio_salida, f"{nombre_banda}.tif")
         # Guarda la banda como un nuevo archivo GeoTIFF
@@ -142,7 +145,9 @@ def desconcatenar_bandas_avion(directorio_imagenes, nombre_archivo, directorio_s
     # Guarda cada banda por separado
     for nombre_banda, banda in zip(['red', 'green', 'blue', 'alpha'], [red, green, blue, alpha]):
         meta_banda = meta.copy()
-        meta_banda.update(count=1)
+        meta_banda.update(
+            count=1
+            )
         # Actualiza el nombre del archivo de salida con el nombre de la banda
         nombre_salida = os.path.join(directorio_salida, f"{nombre_banda}.tif")
         # Guarda la banda como un nuevo archivo GeoTIFF
@@ -178,32 +183,26 @@ def transformacion_radiancia(directorio_dron, directorio_satelite, directorio_de
             imagen_dron = os.path.join(directorio_dron, nombre_archivo_dron)
             with rasterio.open(imagen_dron) as banda_dron:
                 metadata = banda_dron.meta
-                metadata.update(
-                dtype=rasterio.uint8,
-                count=1,
-                compress='lzw',
-                nodata=255,
-                )
                 banda_dron = banda_dron.read(1)
             # Identifica la banda del satélite correspondiente
-            if 'blue' in nombre_archivo_dron:
+            if 'Banda_B' in nombre_archivo_dron:
                 nombre_archivo_satelite = 'recortada_RT_T18NXL_20221224T152641_B02_reprojected.tif'
-            elif 'red' in nombre_archivo_dron:
+            elif 'Banda_R' in nombre_archivo_dron:
                 nombre_archivo_satelite = 'recortada_RT_T18NXL_20221224T152641_B04_reprojected.tif'
-            elif 'green' in nombre_archivo_dron:
+            elif 'Banda_G' in nombre_archivo_dron:
                 nombre_archivo_satelite = 'recortada_RT_T18NXL_20221224T152641_B03_reprojected.tif'
             else:
                 print(f'No se puede identificar la banda correspondiente para {nombre_archivo_dron}')
                 continue
             ruta_archivo_satelite = os.path.join(directorio_satelite, nombre_archivo_satelite)
             # Lee la banda del satélite
-            with rasterio.open(ruta_archivo_satelite) as src:
-                banda_satelite = src.read(1)
-            valor_max_satelite = np.max(banda_satelite, where=(banda_satelite < 255), initial=0)
-            valor_min_satelite = banda_satelite.min()
-            print(valor_min_satelite, valor_max_satelite)
+            banda_satelite = leer_banda(ruta_archivo_satelite)
             # Interpola la banda del dron con la banda correspondiente del satélite
-            banda_interpolada = np.interp(banda_dron, (banda_dron.min(), banda_dron.max()), (valor_min_satelite, valor_max_satelite))
+            valor_max = np.max(banda_satelite, where=(banda_satelite < 255), initial=0)
+            print(valor_max)
+            valor_min = banda_satelite.min()
+            print(valor_min)
+            banda_interpolada = np.interp(banda_dron, (banda_dron.min(), banda_dron.max()), (valor_min, valor_max))
             # Guarda la banda interpolada en el directorio de destino
             ruta_banda_destino = os.path.join(directorio_destino, nombre_archivo_dron)
             # Guarda la banda como un nuevo archivo GeoTIFF
@@ -216,7 +215,15 @@ def transformacion_radiancia(directorio_dron, directorio_satelite, directorio_de
             valores_dron = extraer_valores(ruta_banda_destino, puntos) 
             print("Valores verdaderos (Imagen Satelital):", valores_satelital) 
             print("Valores relativamente ajustados (Imagen UAV):", valores_dron)
-            
+
+def leer_banda(ruta_archivo):
+    with rasterio.open(ruta_archivo) as src:
+        return src.read(1)
+
+def guardar_banda(ruta_archivo, datos_banda):
+    with rasterio.open(ruta_archivo, 'w', driver='GTiff', height=datos_banda.shape[0], width=datos_banda.shape[1], count=1, dtype=datos_banda.dtype) as dst:
+        dst.write(datos_banda, 1)
+
 def crear_imagen_rgb(ruta_b1, ruta_b2, ruta_b3, ruta_destino):
     # Abre cada banda por separado
     with rasterio.open(ruta_b1) as src1, rasterio.open(ruta_b2) as src2, rasterio.open(ruta_b3) as src3:
@@ -226,12 +233,19 @@ def crear_imagen_rgb(ruta_b1, ruta_b2, ruta_b3, ruta_destino):
         banda_3 = src3.read(1)
         # Obtiene metadatos para una de las bandas (en este caso, la banda_1)
         metabanda = src1.meta
-        metabanda.update({
-            "count": 3
-            })
+        metabanda.update(
+                dtype=rasterio.uint8,
+                count=3,
+                )
+    # Agrega impresiones para verificar las dimensiones antes de la concatenación
+    print(f"Dimensiones banda_1: {banda_1.shape}")
+    print(f"Dimensiones banda_2: {banda_2.shape}")
+    print(f"Dimensiones banda_3: {banda_3.shape}")
+
     # Concatena las bandas a lo largo del tercer eje para formar una imagen RGB
-    imagen_unida = np.stack([banda_1, banda_2, banda_3], axis=-1)
-    # Guarda la imagen RGB como un nuevo archivo GeoTIFF
+    imagen_unida = np.dstack([banda_1, banda_2, banda_3])
+    print(f"Dimensiones imagen_unida: {imagen_unida.shape}")
+
     with rasterio.open(ruta_destino, 'w', **metabanda) as dest:
-        # Escribe la imagen RGB en la primera banda del archivo raster
-        dest.write(imagen_unida)
+        for ix in range(imagen_unida.shape[2]):
+            dest.write(imagen_unida[:,:,ix], ix+1)
