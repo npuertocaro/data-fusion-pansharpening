c4a4e94305680f245a0efcf81e4aed7815742c3f
Natalia Puerto <lapuertoc@unal.edu.co>
2024-01-27 08:31:47 -0500

    Organización de comentarios de contexto

--- main_fusion.py
+++ main_fusion.py
@@ -1,142 +1,160 @@
 from func.functions import *
-import matplotlib.pyplot as plt
 from dotenv import load_dotenv, find_dotenv
 import os
 import csv
-from data_fusion.ihs.ihs import rgb_to_ihs
-from data_fusion.a_wavelet.a_wavelet import a_wavelet
-from data_fusion.evaluacion.evaluacion_calidad import test_full_references, test_no_references
 from skimage.exposure import match_histograms
-import cv2
 import numpy as np
 from math import sqrt
+import matplotlib.pyplot as plt
+from data_fusion.ihs.ihs import rgb_to_ihs
+from data_fusion.a_wavelet.a_wavelet import fusion_twa_multiband
+from data_fusion.evaluacion.evaluacion_calidad import (
+    test_full_references,
+    test_no_references,
+    spectral_ERGAS,
+    spatial_ERGAS
+)
 
-#Cargar direcciones (path) y buscar las variables de entorno
+# ========== Carga de Direcciones y Variables de Entorno ==========
+
+print("========== Iniciando Carga de Direcciones y Variables de Entorno ==========")
 load_dotenv()
 
 path_root = os.getcwd()
 dir_file_type = os.getenv("name_folder")
 dir_file_original_images_spectral = os.getenv("direccion_imagenes_ingreso_satelite")
 dir_file_original_images_spatial = os.getenv("direccion_imagenes_ingreso_dron")
-dir_file_proccesed_images = os.path.join(path_root,dir_file_type,'processed_images')
+dir_file_proccesed_images = os.path.join(path_root, dir_file_type, 'processed_images')
 
 spatial_imagen_name = os.getenv("name_image_spatial")
 spectral_imagen_name = os.getenv("name_image_spectral")
 
-#Cargar las imágenes
-spatial_src = read_tif_image(dir_file_original_images_spatial,spatial_imagen_name)
-spectral_src = read_tif_image(dir_file_original_images_spectral,spectral_imagen_name)
-print(spatial_src.shape)
-print(spectral_src.shape)
+# ========== Carga de Imágenes ==========
 
-#Leo la imagen y reesampling, el metadata me guarda los datos originales de la espectral
-print("Leyendo imágenes y haciendo resampling")
-inter_spectral, metadata = resampling_spectral(spectral_src,spatial_src.height,spatial_src.width,'bilinear') 
+spatial_src = read_tif_image(dir_file_original_images_spatial, spatial_imagen_name)
+spectral_src = read_tif_image(dir_file_original_images_spectral, spectral_imagen_name)
+
+# ========== Preprocesamiento de Imágenes ==========
+
+inter_spectral, metadata = resampling_spectral(spectral_src, spatial_src.height, spatial_src.width, 'bilinear') 
 spectral_src.close()
-print(inter_spectral.shape)
 
-print(metadata, spectral_src)
+# ========== Extracción de Bandas y Creación de Falsos Colores ==========
 
-# Cargar las bandas y se guardan como una lista de bandas separadas
-print("Cargar las bandas y se guardan como una lista de bandas separadas")
+print("========== Iniciando Fusión de Datos ==========")
 bands = get_RGB_bands_satellite_ndarray_and_other_bands(inter_spectral)
-[red,green,blue] = bands[0:3]
+[red, green, blue] = bands[0:3]
 other_bands = bands[3:]
-image_rgb = ndarray_rgb(red,green,blue)
-print(image_rgb.shape)
+image_rgb = ndarray_rgb(red, green, blue)
 
-# Creacion de la falsa pancromatica
+# ========== Creación de la Falsa Pancromática ==========
+
 print("Creación de la falsa pancromática")
 pancromatica = get_pancromatica(spatial_src)
 spatial_src.close()
 pancromatica = pancromatica.astype(np.float64)
 
-#RGB TO IHS
+# ========== Conversión RGB a IHS e Igualación de Histogramas ==========
+
 print("RGB a IHS")
 iv1v2 = rgb_to_ihs(image_rgb)
 iv1v2 = iv1v2.astype(np.float64)
 
-#Ecualizar los histogramas
-print("Ecualizar los histogramas")
-pan_i = match_histograms(pancromatica,iv1v2[:,:,0])
-#cv2.imwrite(os.path.join(dir_file_proccesed_images,"Imagen_ecualizada.png"),pan_i.astype('int64'))
-show_images([pancromatica,pan_i,iv1v2[:,:,0]],dir_file_proccesed_images, 'gray')
-show_hist([pancromatica,pan_i,iv1v2[:,:,0]],dir_file_proccesed_images, 'blue')
+print("Igualación los histogramas")
+pan_i = match_histograms(pancromatica, iv1v2[:, :, 0])
+show_images([pancromatica, pan_i, iv1v2[:, :, 0]], dir_file_proccesed_images, 'gray')
+show_hist([pancromatica, pan_i, iv1v2[:, :, 0]], dir_file_proccesed_images, 'blue')
 
-#Preguntar qué método de fusión quiere ejecutar
+# ========== Selección del Método de Fusión ==========
+
 option = 0
-while(True):
-    option = input("Escoja el método de fusión: \n 1. IHS \n 2. Atrous Wavelet \n")
-    if(option == "1" or option == "2"):
+while True:
+    option = input("Escoja el método de fusión:\n 1. IHS\n 2. Atrous Wavelet\n")
+    if option == "1" or option == "2":
         break
     print("Opción incorrecta")
 
 pan_v1_v2 = iv1v2.copy()
 
-if(option == "1"):
-    pan_v1_v2[:,:,0] = pan_i
-elif(option == "2"):
-    N_INT = a_wavelet(pan_i, iv1v2[:,:,0])
-    pan_v1_v2[:,:,0] = N_INT
+if option == "1":
+    # ========== Fusión con Método IHS ==========
 
-#Conversión de IHS a RGB
-print("Conversión de IHS a RGB")
-mask_matrix_to_rgb = [[1 ,1/sqrt(6), 1/sqrt(6)],[1, 1/sqrt(6), -1/2],[1, -2/sqrt(6), 0]]
-merged_rgb_image = process_imag_to_another_model(pan_v1_v2,mask_matrix_to_rgb)
+    pan_v1_v2[:, :, 0] = pan_i
 
-#Guardar datos fusionados
-print("Guardando datos fusionados...")
-merged_rgb_image_8bits = merged_rgb_image.astype('uint8')
-new_spectral_image = merge_bands(merged_rgb_image_8bits,other_bands)
+    print("Conversión de IHS a RGB")
+    mask_matrix_to_rgb = [[1, 1/sqrt(6), 1/sqrt(6)], [1, 1/sqrt(6), -1/2], [1, -2/sqrt(6), 0]]
+    merged_rgb_image = process_imag_to_another_model(pan_v1_v2, mask_matrix_to_rgb)
 
-resultado_name = f"{'wavelet_resultado'if option=='2' else 'ihs_resultado'}_{spectral_imagen_name}"
+    print("Guardando datos fusionados")
+    merged_rgb_image_8bits = merged_rgb_image.astype('uint8')
+    new_spectral_image = merge_bands(merged_rgb_image_8bits, other_bands)
+    resultado_name = f"{'IHS'}_{spectral_imagen_name}_{spatial_imagen_name}"
 
-with rasterio.open(os.path.join(dir_file_proccesed_images,f"{resultado_name}"), 
-                'w',
-                **metadata,
-                ) as dst:
+    with rasterio.open(os.path.join(dir_file_proccesed_images, f"{resultado_name}"),
+                       'w',
+                       **metadata,
+                       ) as dst:
         for ix in range(new_spectral_image.shape[2]):
-            dst.write(new_spectral_image[:,:,ix], ix+1)
+            dst.write(new_spectral_image[:, :, ix], ix+1)
 
-#Cálculo de indices de calidad
+elif option == "2":
+    # ========== Fusión con Método Atrous Wavelet ==========
 
-#Original
-ix_rgb_bands = [3,2,1]
-#Dimension espacial
-spatial_image_src = read_tif_image(dir_file_original_images_spatial,spatial_imagen_name)
+    fused_image_w = fusion_twa_multiband(image_rgb, pan_i, 5)
+
+    print("Guardando datos fusionados")
+    resultado_name = f"{'IHS'}_{spectral_imagen_name}_{spatial_imagen_name}"
+    with rasterio.open(os.path.join(dir_file_proccesed_images, f"{resultado_name}"),
+                       'w',
+                       **metadata,
+                       ) as dst:
+        for ix in range(fused_image_w.shape[2]):
+            dst.write(fused_image_w[:, :, ix], ix+1)
+
+
+# ========== Evaluación de la Calidad de las Imágenes Fusionadas ==========
+
+print("========== Evaluación de la Calidad de las Imágenes Fusionadas ==========")
+# Original
+ix_rgb_bands = [3, 2, 1]
+# Dimensión espacial
+spatial_image_src = read_tif_image(dir_file_original_images_spatial, spatial_imagen_name)
 spatial_imagen = np.dstack([spatial_image_src.read(band) for band in ix_rgb_bands])
-#Dimension espectral
-spectral_image_src = read_tif_image(dir_file_original_images_spectral,spectral_imagen_name)
+# Dimensión espectral
+spectral_image_src = read_tif_image(dir_file_original_images_spectral, spectral_imagen_name)
 spectral_imagen = np.dstack([spectral_image_src.read(band) for band in ix_rgb_bands])
-
-#Resultado
-resultado_image_src = read_tif_image(dir_file_proccesed_images,resultado_name)
+# Resultado
+resultado_image_src = read_tif_image(dir_file_proccesed_images, resultado_name)
 resultado_imagen = np.dstack([resultado_image_src.read(band) for band in ix_rgb_bands])
 
-#Evaluación
-indices_full_reference_spacial = test_full_references(spatial_imagen,resultado_imagen)
-indices_full_reference_spectral = test_full_references(np.transpose(inter_spectral,(1,2,0)),resultado_imagen)
-
-print("Calculo de indices espectrales full reference espacial")
-with open(os.path.join(dir_file_proccesed_images,"indices_calidad_espacial_full_references.csv"), "w", newline="") as f:
+# ERGAS espacial y espectral
+ergas_x = spectral_ERGAS(spatial_imagen, resultado_imagen, 1/2, [1, 1, 1], 3)
+with open(os.path.join(dir_file_proccesed_images, f"{'ergas_espectral'}_{spectral_imagen_name}_{spatial_imagen_name}"), "w", newline="") as f:
     writer = csv.writer(f)
-    for test in indices_full_reference_spacial:
+    for test in ergas_x:
         writer.writerow(test)
 
-print("Calculo de indices espectrales full reference espectral")
-with open(os.path.join(dir_file_proccesed_images,"indices_calidad_espectral_full_references.csv"), "w", newline="") as f:
+ergas_s = spatial_ERGAS(spatial_imagen, np.transpose(inter_spectral, (1, 2, 0)), 1/2, [1, 1, 1], 3)
+with open(os.path.join(dir_file_proccesed_images, f"{'ergas_espacial'}_{spectral_imagen_name}_{spatial_imagen_name}"), "w", newline="") as f:
     writer = csv.writer(f)
-    for test in indices_full_reference_spectral:
+    for test in ergas_x:
         writer.writerow(test)
 
-print(np.transpose(inter_spectral, (1, 2, 0)).shape)
-print(pancromatica.shape)
-print(resultado_imagen.shape)
-
-indices_full_no_reference = test_no_references(np.transpose(inter_spectral,(1,2,0)),pancromatica,resultado_imagen)
-
-print("Calculo de indices espectrales no full reference")
-with open(os.path.join(dir_file_proccesed_images,"indices_calidad_no_full_references.csv"), "w", newline="") as f:
+# Evaluación con librería Sewar
+full_reference_espacial = test_full_references(spatial_imagen, resultado_imagen)
+with open(os.path.join(dir_file_proccesed_images, f"{'full_reference_espacial'}_{spectral_imagen_name}_{spatial_imagen_name}"), "w", newline="") as f:
     writer = csv.writer(f)
-    for test in indices_full_no_reference:
+    for test in full_reference_espacial:
+        writer.writerow(test)
+
+full_reference_espectral = test_full_references(np.transpose(inter_spectral, (1, 2, 0)), resultado_imagen)
+with open(os.path.join(dir_file_proccesed_images, f"{'full_reference_espectral'}_{spectral_imagen_name}_{spatial_imagen_name}"), "w", newline="") as f:
+    writer = csv.writer(f)
+    for test in full_reference_espectral:
+        writer.writerow(test)
+
+full_no_reference = test_no_references(np.transpose(inter_spectral, (1, 2, 0)), pancromatica, resultado_imagen)
+with open(os.path.join(dir_file_proccesed_images, f"{'full_no_reference'}_{spectral_imagen_name}_{spatial_imagen_name}"), "w", newline="") as f:
+    writer = csv.writer(f)
+    for test in full_no_reference:
         writer.writerow(test)
--- main_preprocesamiento.py
+++ main_preprocesamiento.py
@@ -2,36 +2,34 @@ from preprocesamiento.func.functions_pre import *
 from func.functions import *
 from dotenv import load_dotenv
 
-#Cargar direcciones (path) y buscar las variables de entorno
+# ========== Carga de Direcciones y Variables de Entorno ==========
 load_dotenv()
 
 path_root = os.getcwd()
 directorio_originales = os.getenv("ruta_folder_bandas")
 directorio_destino = os.getenv("ruta_folder_bandas_8BIT")
 
-##__________________________________________________-___-__________________________________________________-##
+# ========== Preprocesamiento Imágenes Satelitales ==========
 
-## PREPROCESAMIENTO IMÁGENES SATELITALES ###
-
-# Transformación 8-bit Satelite
+#Transformación 8-bit Satelite
 transformacion_8bit(directorio_originales, directorio_destino)
 print("Transformación de imágenes a 8 bit completado")
 
-# Proyección de EPSG 4326 a EPSG 9377 Satelite
+#Proyección de EPSG 4326 a EPSG 9377
 directorio_8bit = os.getenv("ruta_folder_bandas_8BIT")
 directorio_proyectadas = os.getenv("ruta_del_directorio_transformadas_EPSG")
 epsg_proyeccion = 9377
 reproject_images(directorio_8bit, directorio_proyectadas, epsg_proyeccion)
 print("Reproyección de imágenes a EPSG 9377 completado")
 
-# Recortar imagenes de satelite
+#Recorte imagenes de satelite
 directorio_proyectadas = os.getenv("ruta_del_directorio_transformadas_EPSG")
 directorio_recortadas = os.getenv("ruta_del_directorio_recortadas")
 shapefile_ruta = os.getenv("shapefile_ruta_dron")
 recortar_con_shapefile(directorio_proyectadas, shapefile_ruta, directorio_recortadas)
 print("Imágenes recortadas al área de estudio completo")
 
-# Unir bandas Satelite 2022
+#Concatenación bandas Satelite 
 directorio_recortadas = os.getenv("ruta_del_directorio_recortadas")
 banda_B = os.getenv("banda_B")
 banda_G = os.getenv("banda_G")
@@ -53,39 +51,37 @@ crear_imagen_rgb(banda_3, banda_2, banda_1, direccion_salida_rgb)
 #Resolucion de 10m falso color 832
 crear_imagen_rgb(banda_4, banda_2, banda_1, direccion_salida_fc)
 
-##__________________________________________________-___-__________________________________________________-##
+# ========== Preprocesamiento Imágenes UAS ==========
 
-## PREPROCESAMIENTO DATOS UAV ##
-
-# Proyección de EPSG 4326 a EPSG 9377 UAV
+# Proyección de EPSG 4326 a EPSG 9377
 epsg_proyeccion = 9377
 directorio_uav = os.getenv("ruta_folder_imagenes_uav")
 directorio_proyectadas_uav = os.getenv("ruta_del_directorio_transformadas__uav_EPSG")
 reproject_images(directorio_uav, directorio_proyectadas_uav, epsg_proyeccion)
-print("Reproyección de imágenes UAV a EPSG 9377 completado")
+print("Reproyección de imágenes UAS EPSG 9377 completado")
 
-# #RECORTE IMAGENES UAV
+#Recorte imágenes UAS
 directorio_imagen_uav = os.getenv("ruta_del_directorio_transformadas__uav_EPSG")
 directorio_salida_imagen_uav = os.getenv("ruta_del_directorio_uav_clip")
 shapefile_ruta = os.getenv("shapefile_ruta_dron")
 recortar_con_shapefile(directorio_imagen_uav, shapefile_ruta, directorio_salida_imagen_uav)
 
-#Desconcatenación de bandas uav
+#Desconcatenación de bandas UAS
 directorio_imagen_uav = os.getenv("ruta_del_directorio_transformadas__uav_EPSG")
 nombre_imagen = os.getenv("nombre_imagen")
 directorio_salida_imagen_uav = os.getenv("directorio_salida_imagen_uav")
 desconcatenar_bandas_uav(directorio_imagen_uav, nombre_imagen, directorio_salida_imagen_uav)
-print('Desconcatenación de bandas uav')
+print('Desconcatenación de bandas UAS completado')
 
-# Transformación relativa de radiancia uav
+#Transformación relativa de radiancia UAS
 directorio_dron = os.getenv("directorio_salida_imagen_uav")
 directorio_satelite = os.getenv("ruta_del_directorio_recortadas")
 directorio_destino = os.getenv("directorio_imagen_uav_reflec_correc")
 shapefile_ruta = os.getenv("direccion_shape_correccion_puntos")
 transformacion_radiancia(directorio_dron, directorio_satelite, directorio_destino, shapefile_ruta)
-print('Transformación relativa de radiancia uav')
+print('Transformación relativa de radiancia imágenes UAS completado')
 
-# Unir bandas uav rgb
+#Concatenación bandas UAS
 directorio_destino = os.getenv("directorio_imagen_uav_reflec_correc")
 banda_B = os.getenv("banda_B_uav")
 banda_G = os.getenv("banda_G_uav")
@@ -93,29 +89,44 @@ banda_R = os.getenv("banda_R_uav")
 banda_1_uav = os.path.join(directorio_destino, banda_B)
 banda_2_uav = os.path.join(directorio_destino, banda_G)
 banda_3_uav = os.path.join(directorio_destino, banda_R)
-
 direccion_salida_rgb = os.getenv("direccion_imagenes_ingreso_dron")
 union_uav = os.getenv("name_image_spatial")
 direccion_salida = os.path.join(direccion_salida_rgb, union_uav)
-shapefile_ruta = os.getenv("shapefile_ruta")
-directorio_proyectadas_uav = os.getenv("ruta_del_directorio_transformadas__uav_EPSG")
-
 crear_imagen_rgb(banda_3_uav, banda_2_uav, banda_1_uav, direccion_salida)
 
-##__________________________________________________-___-__________________________________________________-##
+# ========== Preprocesamiento Imágenes UltraCam D ==========
 
-## PREPROCESAMIENTO DATOS AVIÓN ##
-
-## Proyección de EPSG 4326 a EPSG 9377 Avion ##
+#Proyección de EPSG 4326 a EPSG 9377
 epsg_proyeccion = 9377
 directorio_av = os.getenv("ruta_folder_imagenes_avion")
 directorio_proyectadas_av = os.getenv("ruta_del_directorio_transformadas_avion_EPSG")
 reproject_images(directorio_av, directorio_proyectadas_av, epsg_proyeccion)
-print("Reproyección de imágenes avión a EPSG 9377 completado")
+print("Reproyección de imágenes UltraCam D EPSG 9377 completado")
 
-## Desconcatenación de bandas avion
+#Concatenación de bandas UltraCam D
 print("Cargando direc")
 directorio_imagen_avion = os.getenv("ruta_del_directorio_transformadas_avion_EPSG")
 nombre_imagen_avion = os.getenv("nombre_imagen_avion")
 directorio_salida_imagen_avion = os.getenv("directorio_salida_imagen_avion")
 desconcatenar_bandas_avion(directorio_imagen_avion, nombre_imagen_avion, directorio_salida_imagen_avion)
+
+#Transformación relativa de UltraCam D
+directorio_avion = os.getenv("directorio_salida_imagen_avion")
+directorio_satelite = os.getenv("ruta_del_directorio_recortadas")
+directorio_destino_avion = os.getenv("directorio_imagen_avion_reflec_correc")
+shapefile_ruta = os.getenv("direccion_shape_correccion_puntos")
+transformacion_radiancia(directorio_avion, directorio_satelite, directorio_destino_avion, shapefile_ruta)
+print('Transformación relativa de radiancia UltraCam D')
+
+#Concatenación bandas UltraCam D
+directorio_destino_avion = os.getenv("directorio_imagen_avion_reflec_correc")
+banda_B = os.getenv("banda_B_avion")
+banda_G = os.getenv("banda_G_avion")
+banda_R = os.getenv("banda_R_avion")
+banda_1_avion = os.path.join(directorio_destino_avion, banda_B)
+banda_2_avion = os.path.join(directorio_destino_avion, banda_G)
+banda_3_avion = os.path.join(directorio_destino_avion, banda_R)
+direccion_salida_rgb_avion = os.getenv("direccion_imagenes_ingreso_avion")
+union_avion = os.getenv("name_image_spatial")
+direccion_salida = os.path.join(direccion_salida_rgb_avion, union_avion)
+crear_imagen_rgb(banda_3_avion, banda_2_avion, banda_1_avion, direccion_salida)
